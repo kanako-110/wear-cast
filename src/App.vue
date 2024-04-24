f8d419dc99c56d659ce31a15e63cd76a69d03e8c
kanako-110 <kanako11097@gmail.com>
2024-04-24 16:26:45 +0900

    ✨ Feat: installed Vuetify and made home component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<template>aaa</template>
+<template>
+  <!-- 全体のレイアウト。 -->
+  <!-- ヘッダー -->
+  <!-- コンテンツ -->
+  <main>
+    <home-index />
+  </main>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HomeIndex from "./pages/Home/index.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    HomeIndex,
+  },
 });
 </script>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Weather of 4/24</p>
+    <v-btn>post</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeIndex",
+});
+</script>
