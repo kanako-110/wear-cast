9d141bd8a209ff9752162f5838dba2ab1fbe2ae7
kanako-110 <kanako11097@gmail.com>
2024-04-28 17:16:16 +0900

    ✨ Feat; made rought images layout

---
+++ src/pages/Home/components/WearImages.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="wear in wearData"
+      class="d-flex child-flex"
+      :key="wear.id"
+      cols="3"
+    >
+      <v-img
+        :lazy-src="wear.image"
+        :alt="wear.alt"
+        aspect-ratio="4/3"
+        class="bg-grey-lighten-2 h-75"
+        cover
+      />
+      <!--  TODO; loading indicator from https://vuetifyjs.com/en/components/images/#grid-->
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WearImages",
+  setup() {
+    // TODO: fetch wear data
+    const wearData = [
+      {
+        id: 1,
+        image:
+          "https://images.unsplash.com/photo-1619042823674-4f4ad8484b08?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "T-shirt",
+      },
+      {
+        id: 2,
+        image:
+          "https://plus.unsplash.com/premium_photo-1684783848102-7946d8930369?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Shorts",
+      },
+      {
+        id: 3,
+        image:
+          "https://images.unsplash.com/photo-1614533708409-fdad0805a4fa?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Sneakers",
+      },
+      {
+        id: 4,
+        image:
+          "https://images.unsplash.com/photo-1587754539999-b5f5492704c3?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Sneakers",
+      },
+    ];
+    return { wearData };
+  },
+});
+</script>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="my-8 d-flex justify-center">
+  <div class="mt-8 d-flex justify-center">
     <weather-card />
+  </div>
+  <div class="mt-9">
+    <wear-images />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import WeatherCard from "@/pages/Home/components/WeatherCard.vue";
+import WearImages from "@/pages/Home/components/WearImages.vue";
 
 export default defineComponent({
   name: "HomeIndex",
   components: {
     WeatherCard,
+    WearImages,
   },
 });
 </script>
