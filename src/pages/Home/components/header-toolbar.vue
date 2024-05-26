b93f83139330a3e721ecccd3ec55062445ff15f7
kanako-110 <kanako11097@gmail.com>
2024-05-25 17:28:12 -0700

    🩹 Chore: enable opening post modal

--- src/pages/Home/components/header-toolbar.vue
+++ src/pages/Home/components/header-toolbar.vue
@@ -1,16 +1,26 @@
 <template>
   <!-- forecast? -->
   <v-toolbar title="WearCast">
-    <v-btn icon>
+    <v-btn icon @click="dialog = true">
       <v-icon>mdi-pen-plus</v-icon>
     </v-btn>
+
+    <wear-post-dialog v-model="dialog" />
   </v-toolbar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import WearPostDialog from "@/pages/Home/components/wear-post-dialog.vue";
 
 export default defineComponent({
   name: "HeaderToolbar",
+  components: {
+    WearPostDialog,
+  },
+  setup() {
+    const dialog = ref(false);
+    return { dialog };
+  },
 });
 </script>
---
+++ src/pages/Home/components/wear-post-dialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="modelValue" width="auto" persistent>
+    <v-card title="Dialog">
+      <template v-slot:actions>
+        <v-btn class="ms-auto" text="cancel" @click="closeDialog"></v-btn>
+        <v-btn class="ms-auto" text="post" @click="closeDialog"></v-btn>
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WearPostDialog",
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(_, { emit }) {
+    const closeDialog = () => {
+      emit("update:modelValue", false);
+    };
+    return { closeDialog };
+  },
+});
+</script>
