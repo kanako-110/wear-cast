5ff302c8eacacc2f8146d468e9de409918192a7a
kanako-110 <kanako11097@gmail.com>
2024-05-29 21:27:57 -0700

    ✨ Feat; upload outfit image

--- src/pages/Home/components/wear-post-dialog.vue
+++ src/pages/Home/components/wear-post-dialog.vue
@@ -7,6 +7,7 @@
         <v-text-field v-model="input.name" label="Name*" required />
         <v-textarea v-model="input.caption" label="Caption" />
         <!-- required -->
+
         <v-file-input
           v-model="input.image"
           label="Photo Upload*"
@@ -19,16 +20,32 @@
         <v-btn text="cancel" @click="closeDialog"></v-btn>
         <v-btn class="ml-4" text="share" @click="submit"></v-btn>
       </template>
+
+      <!-- TODO; loading layout -->
+      <template v-if="loading" v-slot:append>
+        <v-progress-circular
+          color="primary"
+          indeterminate="disable-shrink"
+          size="16"
+          width="2"
+        ></v-progress-circular>
+      </template>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "@/firebaseConfig";
+import { db, storage } from "@/firebaseConfig";
+import {
+  ref as storageRef,
+  getDownloadURL,
+  uploadBytes,
+} from "firebase/storage";
 
 export default defineComponent({
+  // outfitsに変える
   name: "WearPostDialog",
   props: {
     modelValue: {
@@ -42,24 +59,39 @@ export default defineComponent({
       caption: "",
       image: undefined,
     });
+    const loading = ref(false);
     const closeDialog = () => {
       emit("update:modelValue", false);
     };
 
     const submit = async () => {
+      const file = input.image[0];
+      const uniqueFileName = `${Date.now()}_${Math.random()
+        .toString(36)
+        .substring(2)}_${file.name}`;
+      const storageReference = storageRef(storage, `outfits/${uniqueFileName}`);
+
       try {
-        // TODO; add loading
-        await addDoc(collection(db, "wear_posts"), {
+        loading.value = true;
+        // Upload the file to Firebase Storage
+        console.log({ file });
+        const snapshot = await uploadBytes(storageReference, file);
+
+        // Get the file's download URL
+        const downloadURL = await getDownloadURL(snapshot.ref);
+
+        await addDoc(collection(db, "outfits"), {
           user_name: input.name,
           caption: input.caption,
-          // TODO; image
+          imageUrl: downloadURL,
         });
         closeDialog();
       } catch (e) {
         console.error("Error adding document: ", e);
       }
+      loading.value = false;
     };
-    return { closeDialog, input, submit };
+    return { closeDialog, input, submit, loading };
   },
 });
 </script>
